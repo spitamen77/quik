5817a8427f8389a5f5dfc96687cfaac108b22ef0
spitamen77 <ax5165@gmail.com>
2019-12-07 17:08:55 +0500

    lang323

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+header('Access-Control-Allow-Origin: http://api.tayyora.uz');
 /*
 |--------------------------------------------------------------------------
 | API Routes
