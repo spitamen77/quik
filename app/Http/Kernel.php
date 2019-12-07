c8a611ca00500a1d4b42b5c66c50a33d87e5eb0c
spitamen77 <ax5165@gmail.com>
2019-12-07 16:58:03 +0500

    lang23

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'localization' => \App\Http\Middleware\localization::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 
     /**
