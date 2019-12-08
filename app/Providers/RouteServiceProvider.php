9a6839205b0b2687d78aee570c24b6e98bc75b36
spitamen77 <ax5165@gmail.com>
2019-12-08 11:20:44 +0500

    api prefix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -63,11 +63,17 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
+//    protected function mapApiRoutes()
+//    {
+//        Route::prefix('api')
+//             ->middleware('api')
+//             ->namespace($this->namespace)
+//             ->group(base_path('routes/api.php'));
+//    }
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+        Route::middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }
